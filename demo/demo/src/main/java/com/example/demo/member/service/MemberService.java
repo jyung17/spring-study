528c0d9686b2bc42e34cc27b9b61634226ea43a7
jyung17 <iotaxixi@gmail.com>
2024-11-29 16:33:22 +0900

    :

--- demo/demo/src/main/java/com/example/demo/member/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.member.service;
-
-import com.example.demo.member.dto.MemberDto;
-import java.util.List;
-
-public interface MemberService {
-  public List<MemberDto> getAllMember();
-  public String hello();
-}
